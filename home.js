6fbc98a48a296cf06f0db38be0ff1b6201cbc414
Rsiquette <renan.siquette@gmail.com>
2019-10-17 23:54:41 -0300

    Inserido JS link de paginas/ melhorado hover do home.css

---
+++ home.js
@@ -0,0 +1,22 @@
+function trocaPagina1() {
+    setTimeout(()=> {
+        window.location.href = "portifolio.html";
+    }, 400);    
+}
+
+function trocaPagina2() {
+    setTimeout(()=> {
+        window.location.href = "tecnologias.html";
+    }, 400);    
+}
+
+function trocaPagina3() {
+    setTimeout(()=> {
+        window.location.href = "portifolio.html";
+    }, 400);    
+}
+function trocaPagina5() {
+    setTimeout(()=> {
+        window.location.href = "home.html";
+    }, 400);    
+}
